87b744fa2107f5fdca4a1e3af29ebb7c8c763a0c
Ian-SeoJaeMin <animomin@daum.net>
2025-01-21 22:15:18 +0900

    feat(auth): 인증 모듈 및 관련 구성 요소 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { ConfigModule } from '@modules/@nestjs/config';
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true
-        })
+        }),
+        AuthModule
     ],
     controllers: [],
     providers: []
